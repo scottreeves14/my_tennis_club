dcfffda2fb1a93abaf0bd65e67b35cf3d98a7d0a
Scott Reeves <scott.reeves14@det.nsw.edu.au>
2025-11-13 21:51:42 +0000

    Made find_dups.sql a bit more readable

--- find_dups.sql
+++ find_dups.sql
@@ -1,1 +1,4 @@
-select firstname,lastname,joined_date,phone,count(*) as cnt from members_member group by firstname,lastname,joined_date,phone having count(*) > 1;
+select firstname,lastname,joined_date,phone,count(*) 
+    as cnt from members_member 
+    group by firstname,lastname,joined_date,phone 
+    having count(*) > 1;
